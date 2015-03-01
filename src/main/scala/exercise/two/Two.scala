1045d062c696a10a8aba863514de85c696b0558d
markjamesbutler <markjamesbutler@gmail.com>
2015-03-01 16:51:30 +0000

    uncurry and compose.

--- src/main/scala/exercise/two/Two.scala
+++ src/main/scala/exercise/two/Two.scala
@@ -30,7 +30,15 @@ object Two {
   }
 
   def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
-
+    (a: A, b: B) => f(a)(b)
   }
-
+  
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    (a: A) => f(g(a))
+  }
+ 
+  val f = (x: Double) => math.Pi / 2 -x 
+  
+  val cos = f andThen math.sin
+  
 }
