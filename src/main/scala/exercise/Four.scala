d95d0fe5dfad7a3344c3d528c6c91c6c25736d27
Mark <markjamesbutler@gmail.com>
2015-12-06 17:42:22 +0000

    Chpter 4

---
+++ src/main/scala/exercise/Four.scala
@@ -0,0 +1,41 @@
+package exercise
+
+class Four {
+
+  sealed trait Option[+A]
+  case class Some[+A](get: A) extends Option[A]
+  case object None extends Option[Nothing]
+
+  trait Option[+A] {
+    def map[B](f: A => B): Option[B] =
+    this match {
+        case Some(a) => Some(f(a))
+        case _ => None
+      }
+
+    def flatMap[B](f: A=> Option[B]): Option[B] =
+    this match  {
+      case Some(b) => f(b)
+      case _ => None
+    }
+
+    def getOrElse[B >: A](default: => B): B = {
+      this match {
+        case Some(a) => a
+        case None => default
+      }
+
+    }
+    def orElse[B >: A](ob: => Option[B]) = {
+      this map (Some(_)) getOrElse(ob)
+    }
+
+    def filter(f: A => Boolean): Option[A] = {
+      this match {
+        case Some(a) if f(a) => this
+        case None => None 
+      }
+    }
+  }
+
+}
---
+++ src/test/scala/exercise/FourTest.scala
@@ -0,0 +1,8 @@
+package exercise
+
+import org.scalatest.{Matchers, FlatSpec}
+
+class Four$Test extends FlatSpec with Matchers {
+
+
+}
